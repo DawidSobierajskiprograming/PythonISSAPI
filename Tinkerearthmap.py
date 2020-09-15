3044dc02755eacc979201fb2b0abd1ff9b4aaba3
Dawid <dawidsobierajski@gmail.com>
2020-09-15 01:41:03 +0100

    button prints Hello World to prompt

--- Tinkerearthmap.py
+++ Tinkerearthmap.py
@@ -4,6 +4,11 @@ from tkinter import *
 window = tkinter.Tk()
 
 window.title("GUI") 
+def send_message():
+    print("Hello World!")
+
 label = tkinter.Label(window, text = "- GO FUCK YOUR SELF -").pack()
-button_widget = Button(window, text = "Hello").pack()
+
+button_widget = Button(window, text = "Hello", command = send_message()).pack()
+
 window.mainloop()

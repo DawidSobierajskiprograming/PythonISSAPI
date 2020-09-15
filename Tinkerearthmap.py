e341a066147f2b537865cf1f03a6eef13e81b5a7
Dawid <dawidsobierajski@gmail.com>
2020-09-16 00:42:06 +0100

    Puts a picture on the Tkinter window

--- Tinkerearthmap.py
+++ Tinkerearthmap.py
@@ -3,11 +3,16 @@ from tkinter import *
 from tkinter import messagebox
 
 window = tkinter.Tk()
-
+img = PhotoImage(file='ISSimage.gif')
 window.title("GUI")
 
 def send_message():
     messagebox.showinfo("Hello Python!", "Hello World!")
+
+ISSimage = Canvas(window, width = 680, height = 423)
+ISSimage.pack()
+ISSimage.create_image(20,20, anchor = NW, image = img)
+ISSimage.pack()
 
 label = tkinter.Label(window, text = "- GO FUCK YOUR SELF -").pack()
 

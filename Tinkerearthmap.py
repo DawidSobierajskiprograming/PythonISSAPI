2d7403e350cdefafb1af13bed95774b09e57972b
Dawid <dawidsobierajski@gmail.com>
2020-09-15 01:32:46 +0100

    tkinter button made but dosent do anything

--- Tinkerearthmap.py
+++ Tinkerearthmap.py
@@ -1,7 +1,9 @@
 import tkinter
+from tkinter import *
 
 window = tkinter.Tk()
 
 window.title("GUI") 
-label = tkinter.Label(window, text = "GO FUCK YOUR SELF").pack()
+label = tkinter.Label(window, text = "- GO FUCK YOUR SELF -").pack()
+button_widget = Button(window, text = "Hello").pack()
 window.mainloop()

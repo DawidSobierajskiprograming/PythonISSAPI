9d9f10ed4cb7eeb8ad0218b3334cd2612dee8396
Dawid <dawidsobierajski@gmail.com>
2020-09-15 14:46:55 +0100

    ISS image added and working button

--- Tinkerearthmap.py
+++ Tinkerearthmap.py
@@ -4,12 +4,13 @@ from tkinter import messagebox
 
 window = tkinter.Tk()
 
-window.title("GUI") 
+window.title("GUI")
+
 def send_message():
     messagebox.showinfo("Hello Python!", "Hello World!")
 
 label = tkinter.Label(window, text = "- GO FUCK YOUR SELF -").pack()
 
-button_widget = Button(window, text = "Hello", command = send_message()).pack()
+button_widget = Button(window, text = "Hello", command = send_message).pack()
 
 window.mainloop()
